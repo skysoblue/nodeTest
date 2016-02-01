48a42bd61fb57635b27aec3f264eab60b971867a
skysoblue <coolbeat@naver.com>
2016-02-01 09:43:34 +0900

    angularjs 전환

--- test/app.js
+++ test/app.js
@@ -27,8 +27,11 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+/*app.get('/', routes.index);*/
 app.get('/users', user.list);
+app.get('/',function(req,res){
+	res.sendfile(__dirname+'/views/index.html');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
